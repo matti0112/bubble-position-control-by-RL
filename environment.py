27bdcffd676176ba94a452f1928f0778d6331649
matti0112 <129271438+matti0112@users.noreply.github.com>
2024-05-16 00:10:31 +0200

    Update environment.py

--- environment.py
+++ environment.py
@@ -7,56 +7,116 @@ import pycuda.gpuarray as gpuarray
 import pycuda.driver as cuda
 from pycuda.compiler import SourceModule
 import pycuda.autoinit
+import torch
 
-class CustomEnv(gym.Env):
-    def __init__(self):
-        super(CustomEnv, self).__init__()
-        self.action_space = gym.spaces.Box(low=-1.0, high=1.0, shape=(2,))  # 2D continuous action space
-        self.observation_space = gym.spaces.Box(low=-1.0, high=1.0, shape=(4,))  # 4D continuous observation space
+class CustomEnv(gym.vector.VectorEnv):
+    def __init__(self,
+                 excitation_frequecy: float = 25.0e3,
+                 rest_radius: float = 80.0e-6,
+                 total_steps_per_episode: int = 25,
+                 timet_step_length: float = 10.0,
+                 number_of_parallel_envs: int = 512,
+                 actual_target_position: float = 0.1,
+                 obs_dim: int = 4,
+                 act_dim: int = 2):
+        #self.action_space = gym.spaces.Box(low=-1.0, high=1.0, shape=(2,))  # 2D continuous action space ez így nem jó, PA nem lehet -1
+        action_space = gym.spaces.Box(low=np.array([0.0, 0.0]), high=np.array([1.0*1e5, 0.5 * np.pi]), shape=(2, ))
+        observation_space = gym.spaces.Box(low=np.array([0.0, 0.0, 0.0, 0.0]), high=np.array([1.0, 1.0, 1.0, 1.0]), shape=(4, ) )   #!!!!!!
+        #self.observation_space = gym.spaces.Box(low=-1.0, high=1.0, shape=(4,))  # 4D continuous observation space ez így nem jó, miért lenne minden -1 és 1 között by default..
         # Load the CUDA library
+
+        super(CustomEnv, self).__init__(number_of_parallel_envs, observation_space, action_space)
         self.cuda_lib = ctypes.CDLL('./calculator.so')  # Update with the correct path
-        self.setup()
-        self.reset()
+        self.cuda_lib.call.argtypes = [ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double)]
+        self.cuda_lib.call.restype = ctypes.c_void_p
+
+
+        self.setup(excitation_frequecy, rest_radius)
+        #self.reset()       usually not called here
+
+        # Environment's static features (to avoid hardcoded numbers in member functions)
+        self.total_steps_per_episode = total_steps_per_episode
+        self.timet_step_length       = timet_step_length            # Nem használjuk, de mivel hardcodeolva van a CUDA kóon belül ki kell nézni
+        self.number_of_parallel_envs = number_of_parallel_envs      # Nthreads-el összhangban kell lenni (hardcode a reset stb. loopokban elkerülhetü.)
+        self.actual_target_position  = actual_target_position       # Egyezrübb itt definiálni, mint a rewardon belül keresgélni
+        self.obs_dim=obs_dim
+        self.act_dim=act_dim
+        self.episodic_return=torch.zeros((number_of_parallel_envs,), dtype=np.float64)
+        self.episodic_length=torch.zeros((number_of_parallel_envs,), dtype=np.int64)
+
+    def step_cuda(self, state_gpu, action_gpu):
+        self.cuda_lib.call(ctypes.cast(state_gpu.ptr, ctypes.POINTER(ctypes.c_double)), ctypes.cast(self.cp_gpu.ptr, ctypes.POINTER(ctypes.c_double)), ctypes.cast(action_gpu.ptr, ctypes.POINTER(ctypes.c_double)))
 
     def reset(self):
-        initstate=np.array([1.0,0.25,0.0,0.0],dtype=np.float64)
-        #self.state = (np.random.rand(4)*0.25).astype(np.float64)  # Random state between -1 and 1
-        self.state=np.zeros(512*4,dtype=np.float64)
-        for i in range(512):
-            for j,item in enumerate(initstate):
-                self.state[i*4+j]=item
+        self.initstate=np.array([1.0,0.20,0.0,0.0],dtype=np.float64)
         
+        self.steps_done = torch.zeros((self.number_of_parallel_envs, ), dtype=np.int64)
+        self.rewards_collected = np.zeros((self.number_of_parallel_envs, ), dtype=np.float64)
+
+        self.state=np.tile(self.initstate, self.number_of_parallel_envs)
         return self.state
 
-    def step(self, action):
-        print("Env step started")
-        print(action)
-        # Define the function prototype
-        self.cuda_lib.call.argtypes = [ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double)]
-        self.cuda_lib.call.restype = None
+    def reset_env(self, env_id):
+        self.state[4*env_id:4*env_id+3]=self.initstate
+        self.steps_done[env_id]=0
+        self.episodic_length[env_id]=0
+        self.episodic_return=0.0
+    
+    def get_state(self, env_id: int):
+        if env_id < self.number_of_parallel_envs:
+            return np.array([self.state[env_id*4 + i] for i in range(4)])
+        else:
+            return None
+
+
+    def step(self, action: np.ndarray):
+        # Clip and flatten action
+        action = np.clip(action, self.action_space.low, self.action_space.high).flatten()
+        
         
         # Use PyCUDA to allocate GPU memory
         state_gpu = gpuarray.to_gpu(self.state)
-        cp_gpu = gpuarray.to_gpu(self.cp)
         action_gpu = gpuarray.to_gpu(action)
+
         
-        self.cuda_lib.call(ctypes.cast(state_gpu.ptr, ctypes.POINTER(ctypes.c_double)), ctypes.cast(cp_gpu.ptr, ctypes.POINTER(ctypes.c_double)), ctypes.cast(action_gpu.ptr, ctypes.POINTER(ctypes.c_double)))
+        self.step_cuda(state_gpu, action_gpu)
+        self.steps_done +=1
+        self.episodic_length+=1
         
-        self.state = gpuarray_to_numpy(state_gpu)
-        #print(self.state)
-        #self.state = np.array(state_gpu, dtype=np.float64)
-        reward = np.zeros(512,dtype=np.float64)
-        # Calculate reward based on a combination of state values (modify as needed)
-        for i in range(len(reward)-1):
-            reward[i]=-abs(self.state[i*4+1]-0.1)
-        done = np.full(512,False)
-        info = {}
-        return self.state, reward, done, info
+        
+        
+        self.state = gpuarray_to_numpy(state_gpu) 
+        t_state=torch.reshape(torch.tensor(self.state)(self.obs_dim,self.number_of_parallel_envs))
+
+        # teminated timeouted and their combined -should be [tensor: bool, (num_paralell,)]
+        self.terminated = torch.abs(t_state[1] - self.actual_target_position) < 0.01
+        self.time_out = self.steps_done == self.total_steps_per_episode
+        self.ended=torch.logical_or(self.terminated, self.time_out)
+        
+        
+        reward = self.get_reward()
+        self.episodic_return+=reward
+        
+        
+        # collect the indecies of terminated and timeouted trajectories and reset them
+        dones=torch.nonzero(torch.logical_or(self.terminated, self.time_out)).squeeze()
+        self.reset_env(dones)
+        
+        # create info log
+        info = {"final_observation": t_state[:, self.ended], # collects terminated and timeouted states [torch.tensor: ( obs_dim , num_of_terminated + num_of_timeouted )]
+                "dones": dones, # collected indecies
+                "episodic_return": self.episodic_return[self.ended],
+                "episode_length": self.episodic_length[self.ended]}
+        return t_state, reward, self.terminated, self.time_out, info
+
+
+    def get_reward(self, t_state):
+        return torch.where(self.terminated,-torch.abs(t_state[1]-0.1)+10,-torch.abs(t_state[1]-0.1))
 
     def render(self):
         pass
-    
-    def setup(self):
+
+    def setup(self, excitation_frequecy, rest_radius):
         #  MATERIAL PROPERTIES (SI Untis)
         PV  = 0.0       # Vapour Pressure [Pa]
         RHO = 998.0     # Liquod Density [kg/m**3]
@@ -65,12 +125,13 @@ class CustomEnv(gym.Env):
         CL  = 1500      # Liqid Sound Speed
         P0  = 1.0*1e5   # Ambient Pressure [Pa]
         PE  = 1.4       # Polytrophic Exponent [-]
-        FREQ=31.25e3         # Hz
-        R0=44.8e-6         # [m]
-        
+
+        FREQ=excitation_frequecy    # Hz
+        R0=rest_radius              # [m]
+
         wr = 2.0 * np.pi * FREQ
         lr = CL / FREQ
-        
+
         self.cp = np.zeros((19, ), dtype=np.float64)
         self.cp[0] = (2.0 * ST / R0 + P0 - PV) * pow((2.0 * np.pi / R0 / wr), 2.0) / RHO
         self.cp[1] = (1.0 - 3.0 * PE) * (2.0 * ST / R0 + P0 - PV) * (2.0 * np.pi / R0 / wr) / CL / RHO
@@ -97,18 +158,11 @@ class CustomEnv(gym.Env):
         # Dinamic Constants
         self.cp[17] = wr
         self.cp[18] = 2.0 * np.pi / lr
-        
+
+        self.cp_gpu = gpuarray.to_gpu(self.cp)
+
 def gpuarray_to_numpy(gpu_array):
-    """
-    Copies data from a pycuda GPUArray to a regular NumPy array.
-
-    Args:
-        gpu_array: The GPUArray object to be converted.
-
-    Returns:
-        A NumPy array containing the data from the GPUArray.
-    """
-
+    
     # Get the size and data type of the GPUArray
     shape = gpu_array.shape
     dtype = gpu_array.dtype
@@ -120,18 +174,3 @@ def gpuarray_to_numpy(gpu_array):
     cuda.memcpy_dtoh(host_array, gpu_array.ptr)
 
     return host_array
-
-
-def main():
-    env=CustomEnv()
-    action=np.array([0.225 * 100000.0,0.0]).astype(np.float64)
-    env.reset()
-    for i in range(4):
-        env.step(action)
-        
-    
-    
-    
-
-if __name__=="__main__":
-    main()
